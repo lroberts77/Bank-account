73cce617f714b815a754a63374cfef8504333ab2
liam roberts <liamroberts77@googlemail.comgit config --global user.email liamroberts77@googlemail.com>
2020-04-20 16:13:16 +0100

    added deposit and withdraw with passing tests

---
+++ lib/bankaccount.rb
@@ -0,0 +1,18 @@
+class Bankaccount
+
+    attr_reader :balance
+
+    def initialize
+        @balance = 0
+    end
+
+    def deposit(amount)
+        @balance += amount
+    end
+
+    def withdraw(amount)
+        @balance -= amount
+    end
+
+
+end
---
+++ spec/bankaccount_spec.rb
@@ -0,0 +1,34 @@
+require 'bankaccount.rb'
+
+describe Bankaccount do
+
+# let(:account) {Bankaccount.new}
+
+    it "has a starting balance of zero" do
+        expect(subject.balance).to eq(0)
+    end
+
+    describe "#deposits" do
+
+        it{is_expected.to respond_to(:deposit).with(1).argument}
+
+        it "can deposit money" do
+            expect{subject.deposit (100)}.to change{subject.balance}.by 100
+        end
+    end
+
+    describe "#withdrawal" do
+
+    it{is_expected.to respond_to(:withdraw).with(1).argument}
+
+        it "can withdraw money" do
+            expect{subject.withdraw (100)}.to change{subject.balance}.by -100
+        end
+    end
+
+    
+
+end
+
+
+
